fb115ec6c3518180cdd117c0b7029bb35609d78c
Raposo <112758559+yRaposo@users.noreply.github.com>
2023-04-19 11:26:55 -0300

    Delete node

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const app = express()
+const handlebars = require("express-handlebars").engine
+const bodyParser = require("body-parser")
+const post = require("./models/post")
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get("/", function(req, res){
+    res.render("primeira_pagina")
+})
+
+app.get("/consulta", function(req, res){
+    post.findAll().then(function(post){
+        res.render("consulta", {post})
+    }).catch(function(erro){
+        console.log("Erro ao carregar dados do banco: " + erro)
+    })
+})
+
+app.post("/cadastrar", function(req, res){
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function(){
+        res.redirect("/")
+    }).catch(function(erro){
+        res.send("Falha ao cadastrar os dados: " + erro)
+    })
+})
+
+app.listen(8081, function(){
+    console.log("Servidor ativo!")
+})
